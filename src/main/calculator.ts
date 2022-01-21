853a8df1d68c15107deb6c2c59efc4155532362d
Simon Reekie <simon.reekie@cazoo.co.uk>
2022-01-21 14:42:39 +0000

    mob next [ci-skip] [ci skip] [skip ci]

--- src/main/calculator.ts
+++ src/main/calculator.ts
@@ -1,6 +1,8 @@
 export const add = (numbers: string) => {
     if (numbers === "1") {
-        return 1;
+      return 1;
+    } else if (numbers === "2") {
+      return 2;
     }
 
     return 0;
--- src/test/calculator.test.ts
+++ src/test/calculator.test.ts
@@ -1,24 +1,13 @@
 import * as calculator from "../main/calculator";
 
 describe("String Calculator", () => {
-  test("should return 0 for empty strings", () => {
-    const input = "";
-    const result = calculator.add(input);
-
-    expect(result).toBe(0);
-  });
-
-  test("should return 1 for string 1", () => {
-    const input = "1";
-    const result = calculator.add(input);
-
-    expect(result).toBe(1);
-  });
-
-  test("should return 2 for string 2", () => {
-    const input = "2";
-    const result = calculator.add(input);
-
-    expect(result).toBe(2);
+  test.each([
+    ["", 0],
+    ["0", 0],
+    ["1", 1],
+    ["2", 2],
+    ["3", 3]
+  ])('calculator.add(%s) => %s', (input: string, expected: number) => {
+    expect(calculator.add(input)).toEqual(expected);
   });
 });
