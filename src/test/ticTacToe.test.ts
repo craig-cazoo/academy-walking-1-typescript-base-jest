c80170203a38247d70dd53886f7e3ae6ba6e37a4
furkantopal <topallfurkan@gmail.com>
2022-02-11 15:53:21 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- src/test/ticTacToe.test.ts
+++ src/test/ticTacToe.test.ts
@@ -1,17 +1,24 @@
 enum Player {
-  PlayerX= "player-x"
+  PlayerX = "player-x",
+  PlayerO = "player-o",
 }
 
 class TicTacToe {
   getNextPlayer(): Player {
-    throw new Error('Not implemented');
+    return Player.PlayerX;
+    throw new Error("Not implemented");
   }
+  move(): void {}
 }
 
-describe('TicTacToe', () => {
-  it('should be X to go first', () => {
-    const game = new TicTacToe()
-
-    expect(game.getNextPlayer()).toEqual(Player.PlayerX)
+describe("TicTacToe", () => {
+  it("should be X to go first", () => {
+    const game = new TicTacToe();
+    expect(game.getNextPlayer()).toEqual(Player.PlayerX);
   });
-}); 
+  it("should be O to go second", () => {
+    const game = new TicTacToe();
+    game.move();
+    expect(game.getNextPlayer()).toEqual(Player.PlayerO);
+  });
+});
