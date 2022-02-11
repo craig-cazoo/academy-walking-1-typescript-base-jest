888fb51f3c11f32546d6b3b634f0eadec1655765
Craig Forrest <craig.forrest@cazoo.co.uk>
2022-02-11 15:42:44 +0000

    mob next [ci-skip] [ci skip] [skip ci]

---
+++ src/main/TicTacToe.ts
@@ -0,0 +1,48 @@
+export enum Player {
+  PlayerX = "player-x",
+  PlayerO = "player-o",
+}
+
+
+export enum Position {
+  TopLeft,
+  TopMiddle,
+  TopRight,
+  MiddleLeft,
+  MiddleMiddle
+}
+
+class PositionRepository {
+  private positions: Set<Position> = new Set();
+
+  public addPosition(position: Position) {
+    this.positions.add(position);
+  }
+
+  public hasPosition(position: Position): boolean {
+    return this.positions.has(position);
+  }
+}
+
+export class TicTacToe {
+  private whoIsNext: Player = Player.PlayerX;
+  private playedMoves: PositionRepository = new PositionRepository();
+
+  getNextPlayer(): Player {
+    return this.whoIsNext;
+  }
+
+  move(position: Position): void {
+    if (this.playedMoves.hasPosition(position)) {
+      throw new Error(`the position ${position} has already been played`);
+    }
+
+    this.playedMoves.addPosition(position);
+    this.whoIsNext =
+      this.whoIsNext === Player.PlayerX ? Player.PlayerO : Player.PlayerX;
+  }
+
+  getWinningPlayer() {
+    throw new Error('Not implemented');
+  }
+}
--- src/test/ticTacToe.test.ts
+++ src/test/ticTacToe.test.ts
@@ -1,43 +1,37 @@
-enum Player {
-  PlayerX = "player-x",
-  PlayerO = "player-o",
-}
-
-enum Position {
-  TL = 0,
-}
-
-class TicTacToe {
-  private whoIsNext: Player = Player.PlayerX;
-
-  getNextPlayer(): Player {
-    return this.whoIsNext;
-  }
-
-  move(position: Position): void {
-    this.whoIsNext =
-      this.whoIsNext === Player.PlayerX ? Player.PlayerO : Player.PlayerX;
-  }
-}
+import {Player, Position, TicTacToe} from "../main/TicTacToe";
 
 describe("TicTacToe", () => {
   it.each([
-    [Player.PlayerX, 0],
-    [Player.PlayerO, 1],
-    [Player.PlayerX, 2],
-    [Player.PlayerO, 3],
-  ])("should be %s get to go next after %i moves ", (nextPlayer, moveCount) => {
+    [Player.PlayerX, []],
+    [Player.PlayerO, [Position.TopLeft]],
+    [Player.PlayerX, [Position.TopLeft, Position.TopMiddle]],
+    [Player.PlayerO, [Position.TopLeft, Position.TopMiddle, Position.TopRight]],
+  ])("should be %s get to go next after %i moves ", (nextPlayer, positions: Position[]) => {
     const game = new TicTacToe();
 
-    for (let index = 0; index < moveCount; index++) {
-      game.move(Position.TL);
+    for (const position of positions) {
+      game.move(position);
     }
+
     expect(game.getNextPlayer()).toEqual(nextPlayer);
   });
 
   it("should not be able to play in played position ", () => {
     const game = new TicTacToe();
-    game.move(Position.TL);
-    expect(() => game.move(Position.TL)).toThrowError();
+    game.move(Position.TopLeft);
+    expect(() => game.move(Position.TopLeft)).toThrowError();
   });
-});
+
+  it('player X should win when they have a horizontal line', () => {
+    const game = new TicTacToe();
+
+    game.move(Position.TopLeft);
+    game.move(Position.MiddleLeft);
+    game.move(Position.TopMiddle);
+    game.move(Position.MiddleMiddle);
+    game.move(Position.TopRight);
+
+    expect(game.getWinningPlayer()).toEqual(Player.PlayerX);
+  });
+})
+;
